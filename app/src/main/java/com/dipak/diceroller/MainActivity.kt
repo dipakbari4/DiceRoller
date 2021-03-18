c385a10f4c55d73f5f8d6e54201dd1d764791e61
Dipak Bari <dipakbari4@gmail.com>
2021-03-18 22:09:20 +0530

    View binding added to main activity

--- app/src/main/java/com/dipak/diceroller/MainActivity.kt
+++ app/src/main/java/com/dipak/diceroller/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dipak.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dipak.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // main binding object for main activity
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Intializing binding object by inflating layoutInflater object
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)    // Replace layout reference with binding's root object
+
     }
 }
